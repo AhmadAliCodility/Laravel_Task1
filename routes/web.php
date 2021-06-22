1b65f436da9974a49a295f6bbeac375648c69963
AhmadAliCodility <ahmed.ali@codilitysolutions.com>
2021-06-22 19:13:34 +0500

    first commit

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,7 @@ Route::get('/test', function () {
 
 
 
-//Route::get('/email/verify', function () {
-//    return view('auth.verify-email');
-//})->middleware('auth')->name('verification.notice');
+
 
 
 //todo-----------------------------------__COURSES__------------------------------------------\
